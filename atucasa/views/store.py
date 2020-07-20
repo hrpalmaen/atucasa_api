46e989a0a93bef16b73f07312e85e34302d0d226
cristhian2121 <ccdelgadop@gmail.com>
2020-07-20 11:51:47 -0500

    add limit to query for star in Product and Store

--- atucasa/views/store.py
+++ atucasa/views/store.py
@@ -9,5 +9,12 @@ from rest_framework import viewsets
 class StoreView(viewsets.ModelViewSet):
     ''' stores view '''
     # permission_code = 'store'
-    queryset = Store.objects.all()
+    # queryset = Store.objects.all()
     serializer_class = StoreSerializer
+
+    def get_queryset(self):
+        params =  self.request.query_params
+        star_param = params.get('star', False)
+        if star_param:
+            return Store.objects.all()[:10]
+        return Store.objects.all()
